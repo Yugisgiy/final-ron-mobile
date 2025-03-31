9093fa01702c0ddf66da12c47d0efca0bbb7c77e
Yugisgiy <hetgegeg@gmail.com>
2025-03-31 10:01:59 +0300

    Update FlxHitbox.hx

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -17,8 +17,6 @@ enum FlxHitboxType
  * A zone with 4 hint's (A hitbox).
  * It's really easy to customize the layout.
  */
-important.ClientPrefs();
-
 class FlxHitbox extends FlxSpriteGroup
 {
 	public var buttonLeft:FlxButton = new FlxButton(0, 0);
