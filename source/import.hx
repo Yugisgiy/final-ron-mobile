4094205606d301a797b0b3a6b6e57366746905ca
Yugisgiy <hetgegeg@gmail.com>
2025-03-31 09:25:48 +0300

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,14 +1,3 @@
-//Mobile Controls
-import mobile.objects.MobileControls;
-import mobile.objects.IMobileControls;
-import mobile.objects.Hitbox;
-import mobile.objects.TouchPad;
-import mobile.objects.TouchButton;
-import mobile.input.MobileInputID;
-import mobile.backend.MobileData;
-import mobile.input.MobileInputManager;
-import mobile.backend.TouchUtil;
-
 import important.Paths;
 import important.Conductor;
 import important.ClientPrefs;
@@ -17,4 +6,3 @@ import important.Controls;
 import gameassets.Alphabet;
 import misc.ColorSwap;
 import hxvlc.flixel.*;
-
