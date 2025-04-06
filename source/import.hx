0e4c11c4944bf9122264ef5ab64b9eaa99e23763
Yugisgiy <hetgegeg@gmail.com>
2025-04-06 21:49:36 +0300

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -6,3 +6,8 @@ import important.Controls;
 import gameassets.Alphabet;
 import misc.ColorSwap;
 import hxvlc.flixel.*;
+
+//Mobile Controls
+import mobile.flixel.FlxVirtualPad;
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.input.FlxMobileInputID;
