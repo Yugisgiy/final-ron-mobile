f6c9e7aec6594423d946f86c70cde05e4d03ef2b
Yugisgiy <hetgegeg@gmail.com>
2025-04-19 20:50:15 +0300

    Update MobileOptionsSubState.hx

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -98,7 +98,7 @@ class MobileOptionsSubState extends BaseOptionsMenu
 	{
 		File.saveContent(lime.system.System.applicationStorageDirectory + 'storagetype.txt', ClientPrefs.storageType);
 
-		var lastStoragePath:String = mobile.StorageType.fromStrForce(lastStorageType) + '/';
+		var lastStoragePath:String = StorageType.fromStrForce(lastStorageType) + '/';
 
 		try
 		{
