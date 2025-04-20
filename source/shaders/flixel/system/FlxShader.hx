149d3fd103a68b3e3b9af7b9d1f665ba9d9ea111
Yugisgiy <hetgegeg@gmail.com>
2025-04-20 10:50:21 +0300

    Update FlxShader.hx

--- source/shaders/flixel/system/FlxShader.hx
+++ source/shaders/flixel/system/FlxShader.hx
@@ -73,11 +73,11 @@ class FlxShader extends OriginalFlxShader
 		#if lime_opengles
 		prefix += 'out vec4 output_FragColor;\n';
 		var vertex = prefix
-			+ glVertexSource.replace("attribute", "in")
+			StringTools.replace("attribute", "in")
 				.replace("varying", "out")
 				.replace("texture2D", "texture")
 				.replace("gl_FragColor", "output_FragColor");
-		var fragment = prefix + glFragmentSource.replace("varying", "in").replace("texture2D", "texture").replace("gl_FragColor", "output_FragColor");
+		var fragment = prefix + StringTools.replace("varying", "in").replace("texture2D", "texture").replace("gl_FragColor", "output_FragColor");
 		#else
 		var vertex = prefix + glVertexSource;
 		var fragment = prefix + glFragmentSource;
